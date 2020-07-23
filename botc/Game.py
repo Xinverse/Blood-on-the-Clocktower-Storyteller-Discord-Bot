54fd1fc4f1a29ac3e510e45f7659ef0011b35d29
Xinverse <rad57.2958@gmail.com>
2020-07-23 19:40:52 -0400

    drunk's ego_self now shows in end game stats

--- botc/Game.py
+++ botc/Game.py
@@ -22,6 +22,7 @@ from .Outsider import Outsider
 from .Minion import Minion
 from .Demon import Demon
 from .gamemodes.troublebrewing.Saint import Saint
+from .gamemodes.troublebrewing.Drunk import Drunk
 from .gamemodes.troublebrewing._utils import TroubleBrewing
 from .gamemodes.Gamemode import Gamemode
 from .RoleGuide import RoleGuide
@@ -74,6 +75,7 @@ with open('botc/game_text.json') as json_file:
    evil_wins = strings["gameplay"]["evil_wins"]
    role_reveal = strings["gameplay"]["role_reveal"]
    storyteller_death = strings["lore"]["storyteller_death"]
+   ego_role_reveal = strings["gameplay"]["ego_role_reveal"]
 
 with open('botutils/bot_text.json') as json_file: 
    language = json.load(json_file)
@@ -407,12 +409,26 @@ class Game(GameMeta):
          # Revealing the role list
          role_list_str = ""
          for player in self.sitting_order:
-            short = role_reveal.format(
-               "",
-               player.user.mention, 
-               player.role.true_self.emoji, 
-               player.role.true_self.name
-            ) 
+
+            # The player is a drunk, we use the special reveal short string
+            if player.role.true_self.name == Drunk().name:
+               short = ego_role_reveal.format(
+                  "",
+                  player.user.mention, 
+                  player.role.true_self.emoji, 
+                  player.role.true_self.name,
+                  player.role.ego_self.name
+               ) 
+
+            # The player is not a drunk, we use the default reveal short string
+            else:
+               short = role_reveal.format(
+                  "",
+                  player.user.mention, 
+                  player.role.true_self.emoji, 
+                  player.role.true_self.name
+               ) 
+
             role_list_str += short
             role_list_str += "\n"
 
