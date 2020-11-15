2e6474f2c9eac2dbd454e5cffcffbb30e0d78cb7
nyuszika7h <nyuszika7h@gmail.com>
2020-11-15 14:35:57 +0100

    Fix baron emoji

--- botc/gamemodes/troublebrewing/Baron.py
+++ botc/gamemodes/troublebrewing/Baron.py
@@ -56,7 +56,7 @@ class Baron(Minion, TroubleBrewing, Character, NonRecurringAction):
         self._wiki_link = "http://bloodontheclocktower.com/wiki/Baron"
 
         self._role_enum = TBRole.baron
-        self._emoji = "<:baron:722687671499227167>"
+        self._emoji = "<:tbbaron:777526995843285012>"
     
     def create_n1_instr_str(self):
         """Create the instruction field on the opening dm card"""
