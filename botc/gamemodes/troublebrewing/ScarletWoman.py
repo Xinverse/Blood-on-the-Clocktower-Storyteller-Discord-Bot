81133f5df84ce0a1d3715c229d9672ab4e6098ef
nyuszika7h <nyuszika7h@gmail.com>
2021-01-08 00:15:39 +0100

    Fix import again

--- botc/gamemodes/troublebrewing/ScarletWoman.py
+++ botc/gamemodes/troublebrewing/ScarletWoman.py
@@ -3,9 +3,8 @@
 import json 
 import random
 import discord
-from botc import Townsfolk, Outsider, Minion, Character, NonRecurringAction
+from botc import BOTCUtils, Townsfolk, Outsider, Minion, Character, NonRecurringAction
 from ._utils import TroubleBrewing, TBRole
-import BOTCUtils
 import globvars
 
 with open('botc/gamemodes/troublebrewing/character_text.json') as json_file: 
