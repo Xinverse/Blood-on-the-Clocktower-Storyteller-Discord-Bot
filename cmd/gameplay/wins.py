5436eda17b6de87484f6ef73672d6dc3d1281525
nyuszika7h <nyuszika7h@gmail.com>
2021-01-16 16:48:02 +0100

    Allow usernames to be specified in !wins

--- cmd/gameplay/wins.py
+++ cmd/gameplay/wins.py
@@ -2,19 +2,21 @@
 
 import json
 import sqlite3
+import traceback
 
-from discord import utils
+import discord
 from discord.ext import commands
 
 import botutils
-import globvars
 from ._gameplay import Gameplay
 
 with open("botutils/bot_text.json") as json_file:
     language = json.load(json_file)
 
-wins_nogames_str = language["cmd"]["wins_nogames"]
 wins_str = language["cmd"]["wins"]
+wins_nogames_str = language["cmd"]["wins_nogames"]
+user_not_found_str = language["errors"]["user_not_found"]
+error_str = language["system"]["error"]
 
 
 class Wins(Gameplay, name = language["system"]["gameplay_cog"]):
@@ -29,22 +31,11 @@ class Wins(Gameplay, name = language["system"]["gameplay_cog"]):
         description = language["doc"]["wins"]["description"]
     )
     @commands.check(botutils.check_if_lobby_or_dm_or_admin)
-    async def wins(self, ctx, arg=None):
+    async def wins(self, ctx, user: discord.User = None):
         """Wins command"""
 
-        user = None
-
-        if ctx.message.mentions:
-            user = ctx.message.mentions[0]
-        elif arg:
-            if arg.isnumeric():
-                user = await globvars.client.fetch_user(int(arg))
-        else:
-            user = ctx.author
-
         if not user:
-            await ctx.send("User not found.")
-            return
+            user = ctx.author
 
         with sqlite3.connect("data.sqlite3") as db:
             c = db.execute("SELECT games, wins FROM playerstats WHERE user_id = ?", (user.id,))
@@ -57,6 +48,17 @@ class Wins(Gameplay, name = language["system"]["gameplay_cog"]):
                 games, wins = 0, 0
 
             if games > 0:
-                await ctx.send(wins_str.format(utils.escape_markdown(user.name), wins, games, "" if games == 1 else "s", (wins / games) * 100))
+                await ctx.send(wins_str.format(discord.utils.escape_markdown(user.name), wins, games, "" if games == 1 else "s", (wins / games) * 100))
             else:
-                await ctx.send(wins_nogames_str.format(utils.escape_markdown(user.name)))
+                await ctx.send(wins_nogames_str.format(discord.utils.escape_markdown(user.name)))
+
+    @wins.error
+    async def wins_error(self, ctx, error):
+        if isinstance(error, commands.errors.UserNotFound):
+            await ctx.send(user_not_found_str.format(ctx.author.mention))
+        else:
+            try:
+                raise error
+            except Exception:
+                await ctx.send(error_str)
+                await botutils.log(botutils.Level.error, traceback.format_exc())
