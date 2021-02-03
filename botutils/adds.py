eb9da60bf6a5f20b286962e4324549d308111432
nyuszika7h <nyuszika7h@gmail.com>
2021-02-03 03:06:40 +0100

    Remove roles before unlocking channels

--- botutils/adds.py
+++ botutils/adds.py
@@ -70,7 +70,8 @@ async def remove_all_alive_dead_roles_after_game():
         else:
             await remove_dead_role(player.user)
 
-        for channel_id in LOCK_CHANNELS_SPECIAL_ID:
+    for channel_id in LOCK_CHANNELS_SPECIAL_ID:
+        for player in globvars.master_state.game.sitting_order:
             channel = globvars.client.get_channel(int(channel_id))
             print(f'Unhiding channel {channel} from {player}')
             await channel.set_permissions(player, view_channel=None)
